1a6958eb5468dd087d75560f9a4131a54cde18ba
Pedro Henriqe Belchior Alves <pedro@MacBookPro.bwrouter>
2026-03-17 14:01:17 -0300

    core: start react configuration and install tailwindcss

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <h1 className="text bg-red-300">skdcnskdc</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
